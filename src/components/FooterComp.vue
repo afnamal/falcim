872d3211c01401f5e3d9403123882769bb7baaf3
Abdullah Faruk Namal <afnamal@hotmail.com>
2024-05-06 22:03:00 +0300

    update

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
-    <footer class="bg-dark text-light py-5">
+    <footer class="footer bg-dark text-light py-5">
       <div class="container">
-        <div class="row" style="margin-left: 200px;">
+        <div class="footer-content">
           <!-- Section 1: Navigation Links -->
-          <div class="col-md-4">
+          <div class="footer-column">
             <h5>Kaave</h5>
             <ul class="list-unstyled">
-              <li><a href="#" class="text-light">iPhone ile fal baktır</a></li>
-              <li><a href="#" class="text-light">Android ile fal baktır</a></li>
-              <li><a href="#" class="text-light">Huawei ile fal baktır</a></li>
-              <li><a href="#" class="text-light">Yardım</a></li>
-              <li><a href="#" class="text-light">Kullanım Şartları</a></li>
+              <li><a href="#" class="link-light">iPhone ile fal baktır</a></li>
+              <li><a href="#" class="link-light">Android ile fal baktır</a></li>
+              <li><a href="#" class="link-light">Huawei ile fal baktır</a></li>
+              <li><a href="#" class="link-light">Yardım</a></li>
+              <li><a href="#" class="link-light">Kullanım Şartları</a></li>
             </ul>
           </div>
 
           <!-- Section 2: Language Selection and Info -->
-          <div class="col-md-4">
+          <div class="footer-column">
             <h5>Dünyada Kaave</h5>
             <div class="column">
               <a href="#" class="badge badge-secondary">Türkçe</a><br>
@@ -27,45 +27,79 @@
           </div>
 
           <!-- Section 3: Social Media Links -->
-          <div class="col-md-4">
+          <div class="footer-column">
             <h5>Sosyal Medya</h5>
-            <a href="https://facebook.com" class="badge-primary b-block mb-2"><i class="fab fa-facebook"></i> Facebook</a><br>
-            <a href="https://instagram.com" class="badge-primary b-block mb-2"><i class="fab fa-instagram"></i> Instagram</a><br>
-            <a href="https://twitter.com" class="badge-primary b-block mb-2"><i class="fab fa-twitter"></i> Twitter</a><br>
-            <a href="#" class="badge-primary b-block mb-2"><i class="fas fa-blog"></i> Blog</a><br>
+            <a href="https://facebook.com" class="badge badge-primary mb-2"><i class="fab fa-facebook"></i> Facebook</a><br>
+            <a href="https://instagram.com" class="badge badge-primary mb-2"><i class="fab fa-instagram"></i> Instagram</a><br>
+            <a href="https://twitter.com" class="badge badge-primary mb-2"><i class="fab fa-twitter"></i> Twitter</a><br>
+            <a href="#" class="badge badge-primary mb-2"><i class="fas fa-blog"></i> Blog</a><br>
           </div>
         </div>
+        <div class="copyright">
+          <hr style="width: 70%; margin: 0 auto;">
+          <p class="text-center mt-3">Her hakkı saklıdır. 2024, afnamal</p>
+        </div>
       </div>
-      <div class="copyright" style="margin-bottom: -40px;">
-      <div class="container">
-        <hr style="width: 70%; margin: 0 auto;">
-        <p class="text-center mt-3">Her hakkı saklıdır. 2024, afnamal</p>
-      </div>
-    </div>
     </footer>
-    <!-- Alt çizgi ve telif hakkı metni -->
-   
   </div>
 </template>
 
 <style>
-.btn-outline-light{
-  border: opx;
+:root {
+  --footer-background-color: #343a40;
+  --link-color: aliceblue;
 }
-.badge-primary, .badge{
-  color: aliceblue; text-decoration: none;
+
+.footer {
+  background-color: var(--footer-background-color);
+  color: var(--link-color);
+  padding: 20px 0;
 }
-.text-light{
-  color: aliceblue; text-decoration: none;
+
+.footer-content {
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-start; /* Align items to the top for a cleaner look */
+  flex-wrap: wrap;
+  padding: 0 30px;
+}
+
+.footer-column {
+  flex: 1 1 200px;
+  padding: 10px;
+  text-align: left;
+}
+
+.link-light, .badge-primary, .badge, .social-badge {
+  color: var(--link-color);
+  text-decoration: none;
+}
+
+.social-badge {
+  display: block;
+  margin-bottom: 10px;
 }
 
 .footer hr {
   border-top: 1px solid rgba(255, 255, 255, 0.1);
 }
 
-.footer .copyright {
-  background-color: #343a40;
-  color: #fff;
-  padding: 20px 0;
+@media (max-width: 768px) {
+  .footer-content {
+    flex-direction: column;
+    align-items: flex-start; /* Align content to the left on small screens */
+  }
+
+  .footer-column {
+    margin-bottom: 10px;
+    text-align: left; /* Ensure text is left-aligned on small screens */
+    width: 100%; /* Full width for better control and to prevent wrapping */
+  }
+}
+
+@media (min-width: 992px) {
+  .footer-content {
+    margin-left: 200px; /* Apply margin-left only on screens larger than 992px */
+  }
 }
 </style>
