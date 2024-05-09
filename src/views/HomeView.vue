2a904dab52ee441f4d46e588f318d2dfe6b73094
Abdullah Faruk Namal <afnamal@hotmail.com>
2024-05-10 00:59:35 +0300

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -31,7 +31,7 @@
           <p>{{ $t('features.enterInfo') }}</p>
         </swiper-slide>
         <swiper-slide class="feature-item">
-          <img src="../assets/falcilar21.jpg" alt="Feature 3" @click="pushLogin('/fal')" style="border-radius: 30%;" />
+          <img src="../assets/falcilar22.jpg" alt="Feature 3" @click="pushLogin('/fal')" style="border-radius: 30%;" />
           <p>{{ $t('features.getReading') }}</p>
         </swiper-slide>
       </swiper>
