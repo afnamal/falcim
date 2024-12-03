ebeb009cea87dbaec15b94a9f09a5ee05154d885
afnamal <74655267+afnamal@users.noreply.github.com>
2024-12-03 16:39:06 +0300

    Update notificationService.js

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -2,7 +2,6 @@
 import { createApp } from 'vue';
 import VueToast from 'vue-toast-notification';
 
-// Boş bir Vue uygulaması oluştur ve VueToast plugin'ini kullan
 const app = createApp({});
 app.use(VueToast, {
   position: "top-right",
@@ -18,10 +17,8 @@ app.use(VueToast, {
   rtl: false
 });
 
-// Global toast instance'ını oluştur
 const toast = app.config.globalProperties.$toast;
 
-// Toast fonksiyonlarını dışa aktar
 export function showToast(message, type = 'default') {
   toast[type](message);
 }
